7b46622043e63f3085d1f765642b1d1c95845fab
Matheus Moreira <matheusmoreira1324@hotmail.com>
2022-05-03 15:45:56 -0300

    rest-wordpress

---
+++ footer.php
@@ -0,0 +1,19 @@
+		<footer>
+			<nav class="nav-footer">
+			<?php
+					$args = array(
+						'menu' => 'principal',
+						'container' => false
+					);
+					wp_nav_menu( $args );
+				?>
+			</nav>
+
+			<p><?php bloginfo('name'); ?> © <?php echo date('Y'); ?>. Alguns direitos reservados.</p>
+		</footer>
+
+		<!-- Footer wordpress -->
+		<?php wp_footer(); ?>
+		<!-- fecha Footer wordpress -->
+	</body>
+</html>
